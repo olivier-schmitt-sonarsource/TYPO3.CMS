89ad360ff918e7772a35016e8e7127e37181ddf8
Olivier Schmitt <olivier.schmitt@soanrsource.com>
2020-04-30 09:20:31 +0200

    Remove java

--- Build/types/tablesort.d.ts
+++
@@ -1,7 +0,0 @@
-interface Tablesort {
-}
-
-declare const Tablesort: {
-  new(table: Element, options?: {[key: string]: any}): Tablesort;
-  extend(name: string, pattern: Function, sort: Function): void;
-}
